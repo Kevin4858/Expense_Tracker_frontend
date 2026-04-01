5f7ac2b9575639216dfe6080d6f67d4c2c414b10
Kevin Savaliya <savaliyakevin12@gmail.com>
2026-04-01 10:02:16 +0530

    first commit

---
+++ src/utils/exportUtils.js
@@ -0,0 +1,24 @@
+import * as XLSX from "xlsx";
+
+export const exportToExcel = (data, filename = "transactions") => {
+    if (!data || data.length === 0) {
+        alert("No data to export");
+        return;
+    }
+
+    try {
+        const worksheet = XLSX.utils.json_to_sheet(data);
+        //create a workbook
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Transactions");
+        //Generate a Excel file and trigger download 
+        XLSX.writeFile(workbook, `${filename}.xlsx`, {
+            bookType: "xlsx",
+            type: "array"
+        })
+    } catch (error) {
+        console.error("Error error :",error);
+        alert("Errorn exporting data. Please try again.")
+        
+    }
+}
